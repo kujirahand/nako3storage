5ad36d6bc5224a476be6258158c1d2ac303ad2fb
kujirahand <web@kujirahand.com>
2018-03-29 19:37:13 +0800

    3.0.39に対応

--- app/n3s_lib.inc.php
+++ app/n3s_lib.inc.php
@@ -5,8 +5,8 @@
 // --------------------------------------------------------
 global $n3s_config;
 define("N3S_DB_VERSION", 2);
-define("NAKO_DEFAULT_VERSION", "3.0.38");
-$n3s_config['version'] = 0.38;
+define("NAKO_DEFAULT_VERSION", "3.0.39");
+$n3s_config['version'] = 0.39;
 
 function n3s_getURL($page, $action, $params = array())
 {
