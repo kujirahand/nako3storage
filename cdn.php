f28bc019d2990c1e0bfb0ac03f4d89f1cabd173d
kujirahand <kujira@kujirahand.com>
2020-04-02 19:13:21 +0900

    cdnへのリダイレクトを追加

---
+++ cdn.php
@@ -0,0 +1,24 @@
+<?php
+$DEF_VERSION = '3.0.72';
+// ref) https://www.jsdelivr.com/
+$CDN = 'https://cdn.jsdelivr.net/npm/nadesiko3';
+
+function get($key, $def = '') {
+  if (!isset($_GET[$key])) {
+    return $def;
+  }
+  return $_GET[$key];
+}
+
+
+$ver = get('v', $DEF_VERSION);
+$file = get('f', 'release/wnako3.js');
+
+// 先頭に/があれば削る
+if (substr($file, 0, 1) == '/') {
+  $file = substr($file, 1);
+}
+
+$url = "{$CDN}@{$ver}/{$file}";
+header("location: $url");
+
