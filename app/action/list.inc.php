996b09b84742c4741177e79d706a1a9a7fd33150
kujirahand <kujira@kujirahand.com>
2020-12-08 23:52:49 +0900

    ユーザー名リンクを有効にした

--- app/action/list.inc.php
+++ app/action/list.inc.php
@@ -15,6 +15,7 @@ function n3s_api_list()
         $res[] = array(
             "app_id" => $row['app_id'],
             "title" => $row['title'],
+            "user_id" => $row['user_id'],
             "author" => $row['author'],
             "memo" => $row['memo'],
             "mtime" => $row['mtime'],
@@ -32,6 +33,7 @@ function n3s_list_get()
     $n3s_config['search_word'] = isset($_GET['search_word']) ? $_GET['search_word'] : '';
     $nofilter = empty($_GET['nofilter']) ? 0 : intval($_GET['nofilter']);
     $onlybad = empty($_GET['onlybad']) ? 0 : intval($_GET['onlybad']);
+    $find_user_id = empty($_GET['user_id']) ? 0 : intval($_GET['user_id']);
     // get db
     $db = n3s_get_db();
     // list
@@ -46,6 +48,10 @@ function n3s_list_get()
     if ($onlybad >= 1) { // 通報された投稿のみ表示
         $wheres[] = 'bad > 0';
     }
+    // check user_id
+    if ($find_user_id > 0) {
+        $wheres[] = "user_id = $find_user_id";
+    }
     $statements = array($app_id);
     if (!empty($n3s_config['search_word'])) {
         $wheres[] = 'author = ? OR title LIKE ?';
@@ -54,7 +60,7 @@ function n3s_list_get()
     }
     $wheres[] = 'is_private = 0';
     $statements[] = MAX_APP;
-    $h = $db->prepare('SELECT app_id,title,author,memo,mtime,fav FROM apps ' .
+    $h = $db->prepare('SELECT app_id,title,author,memo,mtime,fav,user_id FROM apps ' .
         ' WHERE ' . implode(' AND ', $wheres) .
         ' ORDER BY app_id DESC LIMIT ?');
     $h->execute($statements);
@@ -65,7 +71,11 @@ function n3s_list_get()
         $id = $row['app_id'];
         if ($min_id >= $id) $min_id = $id - 1;
     }
-    $next_url = n3s_getURL('all', 'list', array('app_id' => $min_id, 'nofilter' => $nofilter));
+    $next_url = n3s_getURL('all', 'list', [
+        'app_id' => $min_id,
+        'nofilter' => $nofilter,
+        'user_id' => $find_user_id,
+    ]);
     if ($app_id === 0) $next_url = ""; // トップなので次はない
     return array(
         "list" => $list,
