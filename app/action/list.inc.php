815b385139f4870c0d821dd0d8b5a33ed8c57b23
kujirahand <kujira@kujirahand.com>
2020-12-08 17:13:50 +0900

    通報機能で値を変更できるように修正

--- app/action/list.inc.php
+++ app/action/list.inc.php
@@ -31,14 +31,20 @@ function n3s_list_get()
     // get parameters
     $n3s_config['search_word'] = isset($_GET['search_word']) ? $_GET['search_word'] : '';
     $nofilter = empty($_GET['nofilter']) ? 0 : intval($_GET['nofilter']);
+    $onlybad = empty($_GET['onlybad']) ? 0 : intval($_GET['onlybad']);
     // get db
     $db = n3s_get_db();
     // list
     $app_id = intval(empty($n3s_config['app_id']) ? 0 : $n3s_config['app_id']);
     if ($app_id <= 0) $app_id = PHP_INT_MAX;
     $wheres = array('app_id <= ?');
-    if ($nofilter < 1) { // has filter ?
+    // check nofilter parameters
+    if ($nofilter < 1) { // パラメータがない場合(通報がないものだけを表示する - 通報機能 #18)
       $wheres[] = 'bad < 2';
+    }
+    // check onlybad parameters
+    if ($onlybad >= 1) { // 通報された投稿のみ表示
+        $wheres[] = 'bad > 0';
     }
     $statements = array($app_id);
     if (!empty($n3s_config['search_word'])) {
@@ -59,7 +65,7 @@ function n3s_list_get()
         $id = $row['app_id'];
         if ($min_id >= $id) $min_id = $id - 1;
     }
-    $next_url = n3s_getURL('all', 'list', array('app_id' => $min_idi, 'nofilter' => $nofilter));
+    $next_url = n3s_getURL('all', 'list', array('app_id' => $min_id, 'nofilter' => $nofilter));
     if ($app_id === 0) $next_url = ""; // トップなので次はない
     return array(
         "list" => $list,
