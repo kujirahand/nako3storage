fa9cf9c6b6074da3cea7b1464408aa293effedf7
kujirahand <kujira@kujirahand.com>
2021-01-14 14:48:42 +0900

    image.php をアップロード

--- image.php
+++ image.php
@@ -1,14 +1,16 @@
 <?php
 
 // id
-$fname = empty($_GET['f']) ? $_GET['f'] : '';
+$fname = empty($_GET['f']) ? '' : $_GET['f'];
 
 // match
-if (!preg_match('/^[a-zA-Z0-9\_\-\.]+$/', $fname)) {
+if (!preg_match('/^([0-9]+)\.([a-zA-Z0-9]+)$/', $fname, $m)) {
   header("HTTP/1.0 404 Not Found");
   echo '404 not found';
   exit;
 }
+$id = $m[1];
+$ext = $m[2];
 
 // check path
 $path = __DIR__.'/images/'.$fname;
@@ -20,7 +22,36 @@ if (!file_exists($path)) {
 
 // アクセスコントロール
 header('Access-Control-Allow-Origin: *');
+header('Content-Type: '.get_mime($ext));
+
+
+
 // output
 readfile($path);
+
+function get_mime($ext) {
+  $mime = [
+    "jpg" => "image/jpeg",
+    "jpeg" => "image/jpeg",
+    "jpe" => "image/jpeg",
+    "gif" => "image/gif",
+    "png" => "image/png",
+    "mp3" => "audio/mpeg",
+    "ogg" => "audio/ogg",
+    "oga" => "audio/ogg",
+    "txt" => "text/plain",
+    "csv" => "text/csv",
+    "tsv" => "text/tsv",
+    "xml" => "text/xml",
+    "json" => "application/json",
+    "js" => "text/javascreipt",
+  ];
+  $ext = strtolower($ext);
+  if (isset($mime[$ext])) {
+    return $mime[$ext];
+  }
+  return "application/octet-stream";
+}
+
 
 
