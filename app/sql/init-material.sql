ee5f7ab1ff4647389926123801bff8d0dfe2ac3a
kujirahand <kujira@kujirahand.com>
2021-05-15 16:17:56 +0900

    メモを追加

--- app/sql/init-material.sql
+++ app/sql/init-material.sql
@@ -3,5 +3,5 @@ CREATE TABLE materials (
   material_id   INTEGER PRIMARY KEY,
   body          TEXT DEFAULT '',
   type          TEXT DEFAULT 'nako3', /* nako3 / text / css / html ... */
-  app_id        INTEGER DEFAULT 0
+  app_id        INTEGER DEFAULT 0 /* 現在 material_id=app_id にした */
 );
