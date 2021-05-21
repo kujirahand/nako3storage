e1a20a61367c949ae50e5124beda5e830dd05b60
kujirahand <kujira@kujirahand.com>
2021-05-21 21:24:44 +0900

    wnakoプログラム以外も書き込めるように #100

---
+++ app/action/plain.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once dirname(__DIR__).'/mime.inc.php';
+require_once __DIR__.'/save.inc.php';
+
+function n3s_web_plain() {
+  $page = intval(isset($_GET['page']) ? $_GET['page'] : 0);
+  if ($page <= 0) {
+    error404(); exit;
+  }
+  $r = db_get1('SELECT * FROM apps WHERE app_id=?', [$page]);
+  if (!$r) {
+    error404(); exit;
+  }
+  if ($r['is_private'] > 0) {
+    error403(); exit;
+  }
+  // ok
+  $b = n3s_getMaterialData($page);
+  if (!$b) {
+    error403(); exit; // broken?
+  }
+  // output
+  $t = $r['nakotype'];
+  if ($t == 'wnako' || $t == 'cnako') {
+    header("Content-Type: text/plain; charset=utf-8");
+    header("Content-Disposition: inline; filename=\"$page\"");
+  } else {
+    $mime = n3s_get_mime($t);
+    header("Content-Type: $mime; charset=utf-8");
+    // header("Content-Disposition: attachment; filename=\"$page\"");
+    header("Content-Disposition: inline; filename=\"$page\"");
+  }
+  echo $b['body'];
+}
+
+function error403() {
+  header("HTTP/1.1 403 Forbidden");
+  echo "403 Forbidden";
+  exit;
+}
+
+function error404() {
+  header("HTTP/1.1 404 Not Found");
+  echo "404 File Not Found...";
+  exit;
+}
+
