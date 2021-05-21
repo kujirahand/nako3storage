d3ac69768095c5f4d6999d06571ce8fcc491509d
kujirahand <kujira@kujirahand.com>
2021-05-21 19:40:34 +0900

    wnakoプログラム以外も書き込めるように #100

--- app/license.inc.php
+++ app/license.inc.php
@@ -15,10 +15,10 @@ $copyright_desc= [
   'N3S_ONLY' => '貯蔵庫のみ/改変不可',
   'CC0' => '著作権破棄',
   'CC-BY-ND' => '著作権表示/改変不可',
-  'MIT' => '<a target="new" href="https://ja.wikipedia.org/wiki/MIT_License">改変可/ライセンス表示</a>',
-  'BSD' => '<a target="new" href="https://ja.wikipedia.org/wiki/BSD%E3%83%A9%E3%82%A4%E3%82%BB%E3%83%B3%E3%82%B9">改変可/ライセンスと著作権表示</a>', 
-  'Apache' => '<a target="new" href="https://ja.wikipedia.org/wiki/Apache_License">改変可/著作権等継承<a>', 
-  'GPL2' => '<a target="new" href="https://ja.wikipedia.org/wiki/GNU_General_Public_License">改変可/ライセンス継承</a>', 
+  'MIT' => '改変可/表示[<a target="new" href="https://ja.wikipedia.org/wiki/MIT_License">詳細</a>]',
+  'BSD' => '改変可/著作権表示[<a target="new" href="https://ja.wikipedia.org/wiki/BSD%E3%83%A9%E3%82%A4%E3%82%BB%E3%83%B3%E3%82%B9">詳細</a>]', 
+  'Apache' => '改変可/著作権継承[<a target="new" href="https://ja.wikipedia.org/wiki/Apache_License">詳細<a>]', 
+  'GPL2' => '改変可/継承[<a target="new" href="https://ja.wikipedia.org/wiki/GNU_General_Public_License">詳細</a>]', 
 ];
 
 
