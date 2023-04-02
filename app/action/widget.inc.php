7691dfe190f1e6fad604950d7a538cd2a89c36aa
kujirahand <web@kujirahand.com>
2023-04-02 17:56:49 +0900

    fixed mute_title

--- app/action/widget.inc.php
+++ app/action/widget.inc.php
@@ -14,16 +14,17 @@ function n3s_web_widget()
     $run = $a['run'] = isset($_GET['run']) ? intval($_GET['run']) : 0;
     $allow = $a['allow'] = isset($_GET['allow']) ? intval($_GET['allow']) : 0;
     $mute_name = $a['mute_name'] = isset($_GET['mute_name']) ? intval($_GET['mute_name']) : 0;
+    $mute_title = $a['mute_title'] = isset($_GET['mute_title']) ? intval($_GET['mute_title']) : 0;
     $page = $a['page'] = isset($_GET['page']) ? intval($_GET['page']) : 0;
     $editkey = isset($_GET['editkey']) ? $_GET['editkey'] : '';
     // sandbox
     $sandbox_url = n3s_get_config('sandbox_url', '');
-    $a['iframe_url'] = "{$sandbox_url}index.php?action=widget_frame&page={$page}&run={$run}&mute_name={$mute_name}&editkey={$editkey}&allow={$allow}";
+    $a['iframe_url'] = "{$sandbox_url}index.php?action=widget_frame&page={$page}&run={$run}&mute_name={$mute_name}&mute_title={$mute_title}&editkey={$editkey}&allow={$allow}";
     // -------------------------------------------------------
     // (互換性のために) 特別扱いする投稿 --- https://bit.ly/3Vpk1RI
     if ($page == 991) {
         $url = $a['iframe_url'];
-        header('location:'.$url);
+        header('location:' . $url);
         echo "<html><body><a href='$url'>$url</a>";
         exit;
     }
