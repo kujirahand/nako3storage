107d0605f4a9bc9ae03d3d7a6842c18b2a2d00e6
kujirahand <web@kujirahand.com>
2023-07-04 11:07:15 +0900

    リンクを調整

--- app/license.inc.php
+++ app/license.inc.php
@@ -14,7 +14,7 @@ $copyright_list = [
 $copyright_desc= [
   '未指定' => '未指定/貯蔵庫のみ',
   '自分用' => '貯蔵庫のみ/転載不可',
-  'N3S_ONLY' => '貯蔵庫のみ/改変不可/<a target="new" href="https://github.com/kujirahand/nadesiko3hub">ハブへ保存許可</a>',
+  'N3S_ONLY' => '貯蔵庫のみ/改変不可/<a target="new" href="https://github.com/kujirahand/nadesiko3hub">ハブ保存可</a>',
   'CC0' => '著作権破棄',
   'CC-BY-ND' => '著作権表示/改変不可',
   'MIT' => '改変可/表示[<a target="new" href="https://ja.wikipedia.org/wiki/MIT_License">詳細</a>]',
