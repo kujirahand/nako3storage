8bb2c7dc4ea5a319711cc76029886a5067f18cb3
kujirahand <web@kujirahand.com>
2023-11-30 14:45:08 +0900

    fixed frame page size

--- widget.php
+++ widget.php
@@ -1,11 +1,18 @@
 <?php
 // 作品ページのショートカット
-$id = (int) ($_SERVER['QUERY_STRING']);
-if ($id === 0) {
-    header('location:./index.php');
-    echo '<a href="index.php">index</a>';
-    exit;
+$id = intval($_SERVER['QUERY_STRING']);
+if ($id <= 0) {
+    $id = empty($_GET['id']) ? 0 : intval($_GET['id']);
+    if ($id <= 0) {
+        $id = empty($_GET['page']) ? 0 : intval($_GET['page']);
+        if ($id <= 0) {
+            header('location:./index.php');
+            echo '<a href="index.php">index</a>';
+            exit;
+        }
+    }
 }
+// 作品ページのショートカット
 $_GET['action'] = 'widget';
 $_GET['page'] = $id;
 include_once('index.php');
