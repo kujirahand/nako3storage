3e2ecc60988cdbe50743b3fdf7604558f6d4b009
kujirahand <web@kujirahand.com>
2023-12-30 01:22:18 +0900

    fixed svg mime

--- app/action/image.inc.php
+++ app/action/image.inc.php
@@ -7,7 +7,6 @@ function n3s_web_image()
 function n3s_api_image()
 {
     $fname = empty($_GET['f']) ? '' : $_GET['f'];
-    
     // match
     if (! preg_match('/^([0-9]+)\.([a-zA-Z0-9]+)$/', $fname, $m)) {
         header("HTTP/1.0 404 Not Found");
