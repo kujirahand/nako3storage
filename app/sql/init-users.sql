0af38d397c274770c69b9bb6ec3fce0d2946c87a
kujirahand <web@kujirahand.com>
2024-01-29 10:35:20 +0900

    usersテーブルのトークンをユニークに変更

--- app/sql/init-users.sql
+++ app/sql/init-users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
   password    TEXT DEFAULT '', /* ハッシュ化して保存 */
   pass_token  TEXT DEFAULT '', /* パスワードリセット用のトークン */
   name        TEXT DEFAULT '', /* ユーザーの名前 */
-  login_token TEXT DEFAULT '', /* ログイン用のトークン */
+  login_token TEXT UNIQUE, /* ログイン用のトークン */
   screen_name TEXT DEFAULT '',
   description TEXT DEFAULT '',
   twitter_id  INTEGER DEFAULT 0,
@@ -12,5 +12,3 @@ CREATE TABLE users (
   ctime       INTEGER DEFAULT 0,
   mtime       INTEGER DEFAULT 0
 );
-
-
