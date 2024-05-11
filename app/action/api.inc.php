da933e48d83e73dfe6a57f83205225b81f7c7a3f
kujirahand <web@kujirahand.com>
2024-05-11 23:48:30 +0900

    貯蔵庫への永続化用のAPIを提供する #194

--- app/action/api.inc.php
+++ app/action/api.inc.php
@@ -49,20 +49,101 @@ function n3s_api_api()
     if (!file_exists($dir_astorage)) {
         api_error('[SYSTEM ERROR] dir_astorage could not write...'); exit;
     }
+    $dbPath = $dir_astorage."/{$user_id}.json";
 
     // astorage_put
     if ($page == 'astorage_put') {
-
+        $data = [];
+        if (file_exists($dbPath)) {
+            $data = json_decode(file_load($dbPath), true);
+        }
+        if (empty($data[$app_id])) {
+            $data[$app_id] = [];
+        }
+        $data[$app_id][$key] = $value;
+        file_save($dbPath, json_encode($data));
+        n3s_api_output(true, ['message' => "saved."]);
+        exit;
     }
 
     // astorage_get
     if ($page == 'astorage_get') {
+        if (!file_exists($dbPath)) {
+            n3s_api_output(true, ['value' => $default]);
+            exit;
+        }
+        $data = json_decode(file_load($dbPath), true);
+        $value = empty($data[$app_id][$key]) ? $default : $data[$app_id][$key];
+        n3s_api_output(true, ['value' => $value]);
+        exit;
     }
 
+    // astorage_keys
+    if ($page == 'astorage_keys') {
+        if (!file_exists($dbPath)) {
+            n3s_api_output(true, ["keys" => []]);
+            exit;
+        }
+        $data = json_decode(file_load($dbPath), true);
+        $kv = empty($data[$app_id]) ? [] : $data[$app_id];
+        $keys = [];
+        foreach ($kv as $key => $val) {
+            $keys[] = $key;
+        }
+        n3s_api_output(true, ['keys' => $keys]);
+        exit;
+    }
+
+    api_error("no page");
 }
 
 function api_error($msg)
 {
-    n3s_api_output(false, ['error' => $msg]);
+    n3s_api_output(false, ['reason' => $msg]);
     exit;
+}
+
+function file_save($filename, $data)
+{
+    // ファイルを書き込みモードで開く
+    $file = fopen($filename, 'w');
+
+    // ファイルロックの取得（排他ロック）
+    if (flock($file, LOCK_EX)) {
+        // データをファイルに書き込む
+        fwrite($file, $data);
+
+        // ロックを解除
+        flock($file, LOCK_UN);
+    } else {
+        // ロックが取得できなかった場合はエラーを表示
+        n3s_api_output(false, ['reason' => 'ロックを取得できませんでした。']);
+    }
+
+    // ファイルを閉じる
+    fclose($file);
+}
+
+function file_load($filename)
+{
+    // ファイルを読み込みモードで開く
+    $file = fopen($filename, 'r');
+
+    // ファイルロックの取得（共有ロック）
+    if (flock($file, LOCK_SH)) {
+        // ファイルからデータを読み込む
+        $data = fread($file, filesize($filename));
+
+        // ロックを解除
+        flock($file, LOCK_UN);
+    } else {
+        // ロックが取得できなかった場合はエラーを表示
+        n3s_api_output(false, ['reason' => 'ロックを取得できませんでした。']);
+        $data = false;
+    }
+
+    // ファイルを閉じる
+    fclose($file);
+
+    return $data;
 }
