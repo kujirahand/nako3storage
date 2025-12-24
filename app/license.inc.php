ccdf89dc2ffa7bbd9a511c0455a415b6fa160f93
kujirahand <web@kujirahand.com>
2025-12-24 23:05:27 +0900

    保存画面の改善

--- app/license.inc.php
+++ app/license.inc.php
@@ -3,22 +3,22 @@ global $copyright_list, $copyright_desc;
 $copyright_list = [
   '未指定' => true,
   'MIT' => true,
-  'N3S_ONLY' => true,
   'CC0' => true,
   'CC-BY-ND' => true,
   'BSD' => true,
   'Apache' => true,
   'GPL2' => true,
+  'N3S_ONLY' => true,
   '自分用' => true,
 ];
 $copyright_desc= [
   '未指定' => '未指定/貯蔵庫のみ',
   'CC0' => '著作権破棄',
   'CC-BY-ND' => '著作権表示/改変不可',
-  'MIT' => '<a target="_blank" href="https://ja.wikipedia.org/wiki/MIT_License">改変可/表示</a>',
-  'BSD' => '<a target="_blank" href="https://ja.wikipedia.org/wiki/BSD%E3%83%A9%E3%82%A4%E3%82%BB%E3%83%B3%E3%82%B9">改変可/著作権表示</a>',
-  'Apache' => '<a target="_blank" href="https://ja.wikipedia.org/wiki/Apache_License">改変可/著作権継承<a>',
-  'GPL2' => '<a target="_blank" href="https://ja.wikipedia.org/wiki/GNU_General_Public_License">改変可/継承</a>',
-  'N3S_ONLY' => '貯蔵庫のみ/転載不可/改変不可/<a target="_blank" href="https://github.com/kujirahand/nadesiko3hub">ハブ保存可</a>',
+  'MIT' => '改変可/表示',
+  'BSD' => '改変可/著作権表示',
+  'Apache' => '改変可/著作権継承',
+  'GPL2' => '改変可/継承',
+  'N3S_ONLY' => '貯蔵庫のみ/転載不可/改変不可/ハブ保存可',
   '自分用' => '貯蔵庫のみ/転載不可/ハブ保存なし',
 ];
